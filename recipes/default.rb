944fdac7291eeccfb0ba62859b90ea7aaba68766
Gavin Staniforth <gsdevme@users.noreply.github.com>
2016-03-22 00:17:05 +0000

    fix(crond) - check if the hash is empty

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,27 +1,29 @@
-node[:crond].each do |name, config|
-  configset = Chef::Mixin::DeepMerge.hash_only_merge(
-    {
-      'day' => '*',
-      'hour' => "*",
-      'minute' => "*",
-      'user' => "root",
-      'shell' => "/bin/bash",
-      'environment' => { 'PATH' => "/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin" }
-    },
-    config
-  )
-
-  unless configset.has_key? 'command'
-    raise "Config must include the command"
-  end
-
-  cron_d "#{name}" do
-    command configset['command']
-    day configset['day']
-    hour configset['hour']
-    minute configset['minute']
-    user configset['user']
-    shell configset['shell']
-    environment configset['environment']
+if !node[:crond].empty?
+  node[:crond].each do |name, config|
+    configset = Chef::Mixin::DeepMerge.hash_only_merge(
+      {
+        'day' => '*',
+        'hour' => "*",
+        'minute' => "*",
+        'user' => "root",
+        'shell' => "/bin/bash",
+        'environment' => { 'PATH' => "/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin" }
+      },
+      config
+    )
+  
+    unless configset.has_key? 'command'
+      raise "Config must include the command"
+    end
+  
+    cron_d "#{name}" do
+      command configset['command']
+      day configset['day']
+      hour configset['hour']
+      minute configset['minute']
+      user configset['user']
+      shell configset['shell']
+      environment configset['environment']
+    end
   end
 end
