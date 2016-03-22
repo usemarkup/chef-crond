f661ebea40d255d01b0cc732478eb3cab0278920
Gavin Staniforth <gsdevme@users.noreply.github.com>
2016-03-22 00:17:24 +0000

    chore(build) - bumps metadata

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ name             'crond'
 license          'MIT Licence'
 description      'Template for chef cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.3'
+version          '0.0.4'
 depends          'cron'
