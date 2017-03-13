b807dcf627b004f391cff261d2188bda3fb2dfbd
Gavin Staniforth <gsdevme@users.noreply.github.com>
2017-03-13 11:29:49 +0100

    chore: don't allow CRON 4.0.0

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ license          'MIT Licence'
 description      'Template for chef cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.4'
-depends          'cron'
+depends          'cron', '< 4.0.0'
