0b0c1a6c7c3b5732055d3c5e9ff6f1016ec7abdb
Gavin Staniforth <gavin@usemarkup.com>
2017-05-24 10:19:57 +0100

    chore: use any version of cron

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ license          'MIT Licence'
 description      'Template for chef cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.4'
-depends          'cron', '< 4.0.0'
+depends          'cron', '~4'
